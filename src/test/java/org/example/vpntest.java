677f5f65ac5c520ea74069b8a4309af2ad20c44a
chath <we@as.sd>
2024-03-31 19:27:22 +0530

    fdvgevf

--- src/test/java/org/example/vpntest.java
+++ src/test/java/org/example/vpntest.java
@@ -58,7 +58,7 @@ public class vpntest {
                 chromeOptions.addArguments("--disable-extensions");
                 //chromeOptions.addArguments("--disable-gpu");
                 chromeOptions.addArguments("--no-sandbox");
-                chromeOptions.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
+                chromeOptions.addArguments("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
                 chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
 
                 chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
